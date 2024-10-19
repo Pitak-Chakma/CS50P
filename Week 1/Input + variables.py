bc2e87d6f958c643dd81c53eb7709c77c0fc72ab
Pitak W <pitakchakmacse@gmail.com>
2024-10-19 18:35:02 +0600

    Create Input + variables.py

---
+++ Week 1/Input + variables.py
@@ -0,0 +1,8 @@
+# input takes in argument itself, so no need to use a print() to display message
+
+# print("What's you name? ")
+input("What's you name? ")
+print("Hello, Pitak")
+
+name = input("What's you name? ")
+print("Hello,",name)
